865b5fc4b7d120be3ac779fd1db00058d6e278d3
caitongxue <675398066@qq.com>
2022-11-14 17:58:40 +0800

    2022年11月14日 17:58:39

--- assets/Index.a152e5dc.js
+++
@@ -1,1 +0,0 @@
-import{k as r,K as s,r as u,o as a,c}from"./index.e17eed5b.js";import{u as n}from"./redPacketStore.90183398.js";const k={__name:"Index",setup(p){r();const e=n(),o=s().params.uuid;return e.uuid=o,(m,i)=>{const t=u("router-view");return a(),c(t)}}};export{k as default};
---
+++ assets/Index.c70e56a3.js
@@ -0,0 +1,1 @@
+import{k as r,K as s,r as u,o as a,c}from"./index.c0ffb222.js";import{u as n}from"./redPacketStore.9868256e.js";const k={__name:"Index",setup(p){r();const e=n(),o=s().params.uuid;return e.uuid=o,(m,i)=>{const t=u("router-view");return a(),c(t)}}};export{k as default};
