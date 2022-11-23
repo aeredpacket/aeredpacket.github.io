16c729121f9b102ca05f9d1f5e3e38bbdba759d5
qiangzhili <1205131727@qq.com>
2022-11-23 11:44:36 +0800

    2022年11月23日11:44:35

--- assets/Index.71c0d555.js
+++
@@ -1,1 +0,0 @@
-import{k as r,P as s,r as u,o as a,c}from"./index.33d95ec2.js";import{u as n}from"./redPacketStore.24b431d8.js";const k={__name:"Index",setup(p){r();const e=n(),o=s().params.uuid;return e.uuid=o,(m,i)=>{const t=u("router-view");return a(),c(t)}}};export{k as default};
---
+++ assets/Index.b21d6ecf.js
@@ -0,0 +1,1 @@
+import{k as r,P as s,r as u,o as a,c}from"./index.8581d43f.js";import{u as n}from"./redPacketStore.0fd5311b.js";const k={__name:"Index",setup(p){r();const e=n(),o=s().params.uuid;return e.uuid=o,(m,i)=>{const t=u("router-view");return a(),c(t)}}};export{k as default};
