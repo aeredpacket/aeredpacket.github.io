3ea35b7415f21cdee109ab7856d58187cd1ab139
qiangzhili <1205131727@qq.com>
2022-11-23 12:48:46 +0800

    2022年11月23日12:48:45

---
+++ assets/Index.f72411b3.js
@@ -0,0 +1,1 @@
+import{k as r,P as s,r as u,o as a,c}from"./index.743596a2.js";import{u as n}from"./redPacketStore.ac5a90d7.js";const k={__name:"Index",setup(p){r();const e=n(),o=s().params.uuid;return e.uuid=o,(m,i)=>{const t=u("router-view");return a(),c(t)}}};export{k as default};
