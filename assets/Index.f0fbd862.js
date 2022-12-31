3b245b6c523463b1eace9c1ef1025833c579babe
ccl <675398066@qq.com>
2022-12-31 23:39:10 +0800

    修改最佳手气的判断方式2

---
+++ assets/Index.f0fbd862.js
@@ -0,0 +1,1 @@
+import{k as r,O as s,r as u,o as a,c}from"./index.6719cf0c.js";import{u as n}from"./redPacketStore.8a8a3507.js";const k={__name:"Index",setup(p){r();const e=n(),o=s().params.uuid;return e.uuid=o,(m,i)=>{const t=u("router-view");return a(),c(t)}}};export{k as default};
