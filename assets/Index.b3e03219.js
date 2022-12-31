c2f3d469b89aba90b0dd06ba1b0fdd050a8e54ee
ccl <675398066@qq.com>
2022-12-31 23:24:14 +0800

    修改红包数量最多为500个

---
+++ assets/Index.b3e03219.js
@@ -0,0 +1,1 @@
+import{k as r,O as s,r as u,o as a,c}from"./index.d6396126.js";import{u as n}from"./redPacketStore.05f7af3f.js";const k={__name:"Index",setup(p){r();const e=n(),o=s().params.uuid;return e.uuid=o,(m,i)=>{const t=u("router-view");return a(),c(t)}}};export{k as default};
