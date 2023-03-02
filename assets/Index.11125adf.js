ed5478954653e42703a6c8519258b1021816f014
caitongxue <675398066@qq.com>
2023-03-02 18:56:08 +0800

    增加手机端的创建红包时的余额不足报错

--- assets/Index.11125adf.js
+++
@@ -1,1 +0,0 @@
-import{k as r,Q as s,r as u,o as c,c as n}from"./index.40bf1fc0.js";import{u as a}from"./redPacketStore.8ac49ee6.js";const f={__name:"Index",setup(p){r();const o=a(),e=s().params.uuid;return o.uuid=e,console.log("uuid: "+e),(i,m)=>{const t=u("router-view");return c(),n(t)}}};export{f as default};
