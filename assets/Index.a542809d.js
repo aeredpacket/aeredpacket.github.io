dbf09e898aa6358ea9aeefaddbd82162f90b072c
qiangzhili <1205131727@qq.com>
2023-12-29 11:57:44 +0800

    更换接口地址

---
+++ assets/Index.a542809d.js
@@ -0,0 +1,1 @@
+import{k as r,R as s,r as u,o as c,c as n}from"./index.037e7445.js";import{u as a}from"./redPacketStore.c09b9662.js";const f={__name:"Index",setup(p){r();const o=a(),e=s().params.uuid;return o.uuid=e,console.log("uuid: "+e),(i,m)=>{const t=u("router-view");return c(),n(t)}}};export{f as default};
